d3f9645cc627cd8cd7b860a5151fe0abfd33fbba
Samir <164155512+Azizovich-373@users.noreply.github.com>
2024-08-13 13:37:30 +0500

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ import { Transaction } from "./components/Tranjaction";
 import { Card } from "./components/Card";
 import { ApiCall } from "./lib/http.request";
 
-const refId = JSON.parse(localStorage.getItem('user'))
-if (refId === null) {
+const local = JSON.parse(localStorage.getItem('user'))
+if (local === null) {
     location.assign('/pages/signin/')
-    return
 }
 const apiCall = new ApiCall(import.meta.env.VITE_BATH_URL)
-const local = JSON.parse(localStorage.getItem('user'))
 const user_name = document.querySelector('#user_name')
 const user_email = document.querySelector('#user_email')
 const body = document.body
@@ -20,11 +18,11 @@ user_email.innerHTML = local.email
 user_name.innerHTML = local["last-name"] + ' ' + local["first-name"]
 reload([{}],body, Header)
 
-apiCall.getData('/wallets?userId=' + refId.id)
+apiCall.getData('/wallets?userId=' + local.id)
     .then(wallet => {
         reload(wallet,card_place, Card) 
     })
-apiCall.getData('/transactions?userId=' + refId.id)
+apiCall.getData('/transactions?userId=' + local.id)
     .then(transaction => {
         reload(transaction,tbody, Transaction)   
     })
